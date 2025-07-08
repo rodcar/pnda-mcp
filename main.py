48b481485f544cdabf8f01914a0f2618d510a437
rodcar <ivanrod12@hotmail.com>
2025-07-08 17:06:52 +0100

    update prompts

--- main.py
+++ main.py
@@ -98,29 +98,47 @@ def generate_questions(topic: str) -> str:
     1. Based on the topic, search for datasets in the PNDA (Plataforma Nacional de Datos Abiertos) Peru.
     2. If no relevant datasets are found, retry the search with a different query.
     3. Based on the search results, get the detailed information of one or more datasets.
-    4. Think hard, generate a list of 5 questions that would be possible to answer with the datasets and it is related to the topic.
-    5. You questions should be based on the datasets and the topic. Do not make up questions.
-    6. Show the questions in a list format do not add extra text or comments.
+    4. Create a temporary jupyter notebook to explore the datasets (Only focus on that objective not others). In particular, explore the data dictionary of the datasets to get the data columns and their descriptions. If it fails to open the data dictionary, skip this step and explore the data headers directly.
+    5. Only continue after you explore the datasets.
+    6. Think hard based on your exploration, generate a list of 5 questions that would be possible to answer with the datasets and columns and it is related to the topic. Justify using the data columns why it would be possible to answer the question (do this for each question).
+    7. You questions should be based on the datasets and the topic. Do not make up questions.
+    8. Show the questions in a list format do not add extra text or comments.
+    9. Delete the temporary jupyter notebook.
     """
 
 @mcp.prompt()
 def analyze_quickly(question: str) -> str:
-    return f"""Answer the following question: {question}
+    return f"""Provide a quick analysis of the following question: {question}
+    Rules:
+    - Do not include or generate synthetic data. All conclusions should be grounded in the data.
+    - Minimal code implementation but you can use Markdwon cells for each section for comments.
+    Objective: Provide a quick analysis of the question but still with a high quality.
+    Use emoticons appropiately to make the analysis more engaging.
     Follow these steps to complete the task:
     1. Based on the question, search for datasets in the PNDA (Plataforma Nacional de Datos Abiertos) Peru that help you answer the question.
     2. If no relevant datasets are found, retry the search with a different query.
     3. Based on the search results, get the detailed information of one or more datasets.
     4. Think hard how to answer the question using the dataset or datasets.
-    5. Create a jupyter notebook with one two code cells: one for the installations and the other for the code.
-    6. The code should be in python. Implement the code to answer the question using the datasets in the code cell. You can create temporal cells to explore the data, but you should delete them at the end.
-    7. Run the code cell to answer the question. Check for error, if error then fix, and execute again the same cell, do until the whole notebook runs correctly and the answer is addressed.
-    8. Check you only worked on a single code cell.
+    5. Create a jupyter notebook, start with the following structure: title, description, imports, code cells.
+    6. Start with the installations.
+    7. Explore the data dictionary of the datasets to get the data columns and their descriptions. If it fails to open the data dictionary, skip this step.
+    8. If not data dictionary is available or not possible to open, explore the dataset to get the data columns.
+    9. Think hard how to answer the question using the dataset or datasets taking into account the data dictionary.
+    10. The code should be in python. Implement the code to answer the question using the datasets in the code cell. You can create temporal cells to explore the data, but you should delete them at the end.
+    11. You could include a graph, and a brief analysis.
+    11. Run the code cell to answer the question. Check for error, if error then fix, and execute again the same cell, do until the whole notebook runs correctly and the answer is addressed.
+    12. Refine the notebook to make simple sections but high-quality. Title, description, imports, main code cell, graphs, answer.
     """
 
 # Execute the notebook. Check for error, if error then fix, and execute again, do until the whole notebook runs correctly
+# Claude Sonnet 4 is the best model for this task.
+# GPT 4.1 produces more straight forward answers.
 @mcp.prompt()
 def full_analysis(question: str) -> str:
-    return f"""Answer the following question: {question}
+    return f"""Provide a full analysis of the following question: {question}
+    Rules:
+    - Do not include or generate synthetic data. All conclusions should be grounded in the data.
+    Use emoticons appropiately to make the analysis more engaging.
     Follow these steps to complete the task:
     1. Based on the question, search for datasets in the PNDA (Plataforma Nacional de Datos Abiertos) Peru that help you answer the question.
     2. If no relevant datasets are found, retry the search with a different query.
